f8229a354e57a157a02e6d076d07faf9db2169b2
Your Name <you@example.com>
2024-07-30 19:56:32 -0300

    fix edit mobile

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { ArrowLeftIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 import { useParams, useRouter } from 'next/navigation'; // Usa useParams para obtener parámetros
 import { useState, useEffect } from 'react';
 
@@ -51,96 +53,105 @@ const EditProject = () => {
   if (loading) return <p>Loading...</p>;
 
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center mt-2">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-2xl">
-        <h2 className="text-2xl font-bold mb-6">Create Project</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="projectName" className="block text-gray-700">
-              Project name
-            </label>
-            <input
-              type="text"
-              id="projectName"
-              name="projectName"
-              value={project.projectName}
-              onChange={handleChange}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="description" className="block text-gray-700">
-              Description
-            </label>
-            <input
-              type="text"
-              id="description"
-              name="description"
-              value={project.description}
-              onChange={handleChange}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="projectManager" className="block text-gray-700">
-              Project manager
-            </label>
-            <select
-              id="projectManager"
-              name="projectManager"
-              value={project.projectManager}
-              onChange={handleChange}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              required
-            >
-              <option value="">Select a person</option>
-              <option value="person1">Person 1</option>
-              <option value="person2">Person 2</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="assignedTo" className="block text-gray-700">
-              Assigned to
-            </label>
-            <select
-              id="assignedTo"
-              name="assignedTo"
-              value={project.assignedTo}
-              onChange={handleChange}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              required
-            >
-              <option value="">Select a person</option>
-              <option value="person1">Person 1</option>
-              <option value="person2">Person 2</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="status" className="block text-gray-700">
-              Status
-            </label>
-            <select
-              id="status"
-              name="status"
-              value={project.status}
-              onChange={handleChange}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            >
-              <option value="Enabled">Enabled</option>
-              <option value="Disabled">Disabled</option>
-            </select>
-          </div>
-          <button
-            type="submit"
-            className="w-30 bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-          >
-            Save changes
-          </button>
-        </form>
+    <>
+      <div className="flex items-center py-4 mb-5 bg-white">
+        <Link href="/dashboard" className="flex items-center text-gray-700 hover:text-gray-900">
+          <ArrowLeftIcon className="md: h-5 w-5 ml-5 " /> <span>Back</span>
+          <span className='ml-3 font-bold'>Edit project</span>
+        </Link>
       </div>
-    </div>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center mt-2">
+        <div className="bg-white p-8 rounded shadow-md w-full max-w-2xl">
+          <div className="bg-white">
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="projectName" className="block text-gray-700">
+                Project name
+              </label>
+              <input
+                type="text"
+                id="projectName"
+                name="projectName"
+                value={project.projectName}
+                onChange={handleChange}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="description" className="block text-gray-700">
+                Description
+              </label>
+              <input
+                type="text"
+                id="description"
+                name="description"
+                value={project.description}
+                onChange={handleChange}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="projectManager" className="block text-gray-700">
+                Project manager
+              </label>
+              <select
+                id="projectManager"
+                name="projectManager"
+                value={project.projectManager}
+                onChange={handleChange}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                required
+              >
+                <option value="">Select a person</option>
+                <option value="person1">Person 1</option>
+                <option value="person2">Person 2</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="assignedTo" className="block text-gray-700">
+                Assigned to
+              </label>
+              <select
+                id="assignedTo"
+                name="assignedTo"
+                value={project.assignedTo}
+                onChange={handleChange}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                required
+              >
+                <option value="">Select a person</option>
+                <option value="person1">Person 1</option>
+                <option value="person2">Person 2</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="status" className="block text-gray-700">
+                Status
+              </label>
+              <select
+                id="status"
+                name="status"
+                value={project.status}
+                onChange={handleChange}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              >
+                <option value="Enabled">Enabled</option>
+                <option value="Disabled">Disabled</option>
+              </select>
+            </div>
+            <button
+              type="submit"
+              className="w-30 bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+            >
+              Save changes
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/app/components/tables/Table.jsx
+++ src/app/components/tables/Table.jsx
@@ -100,7 +100,7 @@ const Table = ({ data }) => {
                                 {open === item.id && (
                                     <div className="absolute right-0 top-12 z-10 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
                                         <div className="p-1">
-                                            <Link href={`/editProject/${item.id}`} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Edit</Link>
+                                            <Link href={`/${item.id}}`} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Edit</Link>
                                             <button className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Delete</button>
                                         </div>
                                     </div>
