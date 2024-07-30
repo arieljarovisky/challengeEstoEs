3a646214687579a3b4a574c0995194ec02ec35b7
Your Name <you@example.com>
2024-07-30 17:14:45 -0300

    fix navbar

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav className="bg-white divide-y divide-solid">
+        <nav className="bg-white">
             <div className="flex px-4 py-4 items-center">
                 <Image
                     className="relative ml-[5%] dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -13,19 +13,6 @@ const Navbar = () => {
                     height={37}
                     priority
                 />
-
-            </div>
-            <div className='flex'>
-                <div className='justify-items-start px-4 py-4 ml-[4%] ' >
-                    <h1 className="text-lg font-semibold">My projects</h1>
-                </div>
-                <div className='ml-auto my-auto mr-[5%]	'>
-                    <Link href="/add-project">
-                        <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">
-                            + Add project
-                        </button>
-                    </Link>
-                </div>
             </div>
             <hr />
         </nav>
