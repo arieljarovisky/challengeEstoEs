bfe08da1086ba54b61688324d446665ee98df83a
Your Name <you@example.com>
2024-07-30 23:31:18 -0300

    search

--- src/app/components/tables/Table.jsx
+++ src/app/components/tables/Table.jsx
@@ -6,14 +6,34 @@ import Link from 'next/link';
 
 const Table = ({ data }) => {
     const [open, setOpen] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
 
     const handleToggle = (id) => {
         setOpen(open === id ? null : id);
     };
 
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredData = data.filter((item) =>
+        item.projectName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.projectManager.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.assignedTo.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.status.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
         <div className="md:px-[5%] py-[3%]">
+            <div className="mb-4">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleSearch}
+                    className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
             <div className="overflow-x-auto">
                 <table className="min-w-full divide-y divide-gray-200 hidden md:table">
                     <thead className="bg-gray-50">
@@ -26,11 +46,11 @@ const Table = ({ data }) => {
                         </tr>
                     </thead>
                     <tbody className="bg-white divide-y divide-gray-200">
-                        {data.map((item, index) => (
+                        {filteredData.map((item) => (
                             <tr key={item.id}>
                                 <td className="px-6 py-4 whitespace-nowrap ">
                                     <p className='font-md text-gray-900'>{item.projectName}</p>
-                                    <p className='text-xs text-gray-500'>Creation date:  09/09/2020 10:30 am</p>
+                                    <p className='text-xs text-gray-500'>Creation date: {item.creationDateTime}</p>
                                 </td>
                                 <td className="px-6 py-4  whitespace-nowrap text-sm text-gray-500 ">
                                     <p className='my-auto'>{item.projectManager}</p>
@@ -70,11 +90,10 @@ const Table = ({ data }) => {
                 </table>
                 <div className="md:hidden">
                     <div className="bg-white mt-5">
-                        {data.map((item, index) => (
-                            <div className="relative border border-gray-200 p-4 w-full">
-
+                        {filteredData.map((item, index) => (
+                            <div className="relative border border-gray-200 p-4 w-full" key={item.id}>
                                 <button
-                                    onClick={() => handleToggle(item.id)} // Usar el ID del proyecto
+                                    onClick={() => handleToggle(item.id)}
                                     className="absolute top-2 right-2 flex items-center p-2 text-gray-500 hover:text-gray-700"
                                 >
                                     <EllipsisVerticalIcon className="h-5 w-5" />
@@ -83,7 +102,7 @@ const Table = ({ data }) => {
                                     <div className="text-gray-700 ">
                                         <div className="text-gray-700 mb-1">{item.projectName}</div>
                                         <div className="text-gray-400">
-                                            <span className="text-[12px]">Creation date: 09/09/2020 10:30 am</span>
+                                            <span className="text-[12px]">Creation date: {item.creationDateTime}</span>
                                         </div>
                                         <div className="flex items-center mt-2">
                                             <Image
